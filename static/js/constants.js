3268d9e706bbd3cd612de4f9ddf624d0ea27615e
Suyog Savalkar <68846539+Suyog-23@users.noreply.github.com>
2021-10-25 00:04:01 +0530

    Update constants.js

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "https://localhost:443/webhooks/rest/webhook";
+const rasa_server_url = "https://64.227.180.205:443/webhooks/rest/webhook";
 const sender_id = uuidv4();
